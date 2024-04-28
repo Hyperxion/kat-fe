2a7d4d1279546293a04dbdffc179ce294016cb51
Erik Demko <erik.demko162@gmail.com>
2024-04-28 18:18:46 +0200

    added image with text on right

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -14,6 +14,16 @@ export function IntroSection() {
           blandit bibendum a a ante. Sed ut aliquam urna. Aenean et mollis lorem. Nullam a arcu nec ipsum varius
           bibendum.
         </p>
+        <img src={introImage} alt="Introduction" className="intro-image" />
+        <p className="intro-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus efficitur ex non tortor blandit commodo.
+          Mauris non leo at leo dictum posuere. Donec fermentum tortor vel nulla tempus, id maximus purus faucibus.
+          Nulla facilisi. Sed non metus sit amet justo iaculis volutpat. Nulla facilisi. Proin rutrum tellus in magna
+          fermentum, quis volutpat ex tristique. Sed dapibus posuere risus, in fermentum elit. Sed ac nisl at odio
+          blandit bibendum a a ante. Sed ut aliquam urna. Aenean et mollis lorem. Nullam a arcu nec ipsum varius
+          bibendum.
+        </p>
+
       </div>
     </div>
   );
