dbbb55ba682a73e6d80124e944e5727e69c1f01e
Erik Demko <erik.demko162@gmail.com>
2024-05-02 10:22:41 +0200

    edited images, daded thumbnails

--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import img1 from '/img2.png';
 
 const images = [];
 
 for (let i = 1; i < 12; i++) {
-    images.push(`/img${i}.png`);    
+  images.push(`/gallery/img${i}.png`);
 }
 
 console.log(`-----> images is: ${JSON.stringify(images, null, 2)}`);
 
 export function GallerySection() {
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -26,12 +25,8 @@ export function GallerySection() {
   };
 
   return (
-    <div className="gallery-section">
-      <img
-        src={images[3]}
-        alt={`Image ${currentIndex + 1}`}
-        className='gallery-image'
-      />
+    <div className="gallery-image">
+      <img src={images[3]} alt={`Image ${currentIndex + 1}`} />
     </div>
   );
 }
