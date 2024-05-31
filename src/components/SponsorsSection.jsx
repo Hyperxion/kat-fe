e0845ec86b68069d9b75718217ef5bdb04c4b0eb
Erik Demko <erik.demko162@gmail.com>
2024-05-31 21:39:09 +0200

    added anareus sponsor

--- src/components/SponsorsSection.jsx
+++ src/components/SponsorsSection.jsx
@@ -2,7 +2,10 @@ export function SponsorsSection() {
   return (
     <div className="image-wrapper" >
       <a href="https://www.stavebne-rezivo.sk" target="_blank" title="Stavebné rezivo, drevené brikety, OSB dosky">
-        <img src="https://www.stavebne-rezivo.sk/images/retrabaner.gif" />
+        <img src="https://www.stavebne-rezivo.sk/images/retrabaner.gif" className="sponzor-logo"/>
+      </a>
+      <a href="https://www.anareus.cz" target="_blank" title="Anareus">
+        <img src="/anareus-logo.png" className="sponzor-logo" />
       </a>
     </div>
   );
